96b6db95f304d41f77ddb10fb7f0e528e163df93
Mohammad Hajeer <mohammadhajeer002@gmail.com>
2024-10-05 08:13:08 +0300

    Fix merge conflicts

--- branch.js
+++ branch.js
@@ -1,4 +1,8 @@
 console.log("New Branch")
 console.log("BREAK THE CODE")
 
+// This code will be executed when the branch is checked out
+// Path: master.js
+// Compare this snippet from salam.js:
+
 console.log("FIXED THE BUG")
